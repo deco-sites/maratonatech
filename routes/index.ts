c6b796e8652737b1a23d86ad324ea16f66108ea2
Guilherme Rodrigues <gui@deco.cx>
2023-04-10 14:47:55 -0300

    Make async

--- routes/index.ts
+++ routes/index.ts
@@ -1,8 +1,9 @@
 import { Handlers } from "$fresh/server.ts";
 
 export const handler: Handlers = {
-  GET: () => {
-    return new Response(Deno.readFileSync("./static/index.html"), {
+  GET: async () => {
+    const html = await Deno.readFile("./static/index.html");
+    return new Response(html, {
       headers: {
         "content-type": "text/html; charset=utf-8",
       },
