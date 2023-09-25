99b0cd8f487a392a01fe7b12c97e7b0a4d60d0b4
Lucas Silvestre <lucas_silvestre.10@hotmail.com>
2023-09-25 17:56:49 -0300

    fix: fresh route

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -10,49 +10,48 @@ import * as $4 from "./routes/conteudos/F2N2-ciclo6.ts";
 import * as $5 from "./routes/conteudos/F2N2-ciclo7.ts";
 import * as $6 from "./routes/conteudos/F2N2-ciclo8.ts";
 import * as $7 from "./routes/conteudos/F2N3-ciclo1.ts";
-import * as $8 from "./routes/conteudos/F2N3-ciclo2.ts";
-import * as $9 from "./routes/conteudos/F2N3-ciclo3.ts";
-import * as $10 from "./routes/conteudos/F2N3-ciclo4.ts";
-import * as $11 from "./routes/conteudos/F2N3-ciclo5.ts";
-import * as $12 from "./routes/conteudos/F2N3-ciclo6.ts";
-import * as $13 from "./routes/conteudos/F2N3-ciclo7.ts";
-import * as $14 from "./routes/conteudos/F2N3-ciclo8.ts";
-import * as $15 from "./routes/fase2-repositorio.ts";
-import * as $16 from "./routes/hq/F2N1-ciclo1.ts";
-import * as $17 from "./routes/hq/F2N1-ciclo2.ts";
-import * as $18 from "./routes/hq/F2N1-ciclo3.ts";
-import * as $19 from "./routes/hq/F2N1-ciclo4.ts";
-import * as $20 from "./routes/hq/F2N1-ciclo5.ts";
-import * as $21 from "./routes/hq/F2N1-ciclo6.ts";
-import * as $22 from "./routes/hq/F2N1-ciclo7.ts";
-import * as $23 from "./routes/hq/F2N1-ciclo8.ts";
-import * as $24 from "./routes/infografico/F2N1-ciclo1.ts";
-import * as $25 from "./routes/infografico/F2N1-ciclo2.ts";
-import * as $26 from "./routes/infografico/F2N1-ciclo3.ts";
-import * as $27 from "./routes/infografico/F2N1-ciclo4.ts";
-import * as $28 from "./routes/infografico/F2N1-ciclo5.ts";
-import * as $29 from "./routes/infografico/F2N1-ciclo6.ts";
-import * as $30 from "./routes/infografico/F2N1-ciclo7.ts";
-import * as $31 from "./routes/infografico/F2N1-ciclo8.ts";
-import * as $32 from "./routes/infografico/F2N2-ciclo1.ts";
-import * as $33 from "./routes/infografico/F2N2-ciclo2.ts";
-import * as $34 from "./routes/infografico/F2N2-ciclo3.ts";
-import * as $35 from "./routes/infografico/F2N2-ciclo4.ts";
-import * as $36 from "./routes/infografico/F2N2-ciclo5.ts";
-import * as $37 from "./routes/infografico/F2N2-ciclo6.ts";
-import * as $38 from "./routes/infografico/F2N2-ciclo7.ts";
-import * as $39 from "./routes/infografico/F2N2-ciclo8.ts";
-import * as $40 from "./routes/infografico/F2N3-ciclo1.ts";
-import * as $41 from "./routes/infografico/F2N3-ciclo2.ts";
-import * as $42 from "./routes/infografico/F2N3-ciclo3.ts";
-import * as $43 from "./routes/infografico/F2N3-ciclo4.ts";
-import * as $44 from "./routes/infografico/F2N3-ciclo5.ts";
-import * as $45 from "./routes/infografico/F2N3-ciclo6.ts";
-import * as $46 from "./routes/infografico/F2N3-ciclo7.ts";
-import * as $47 from "./routes/infografico/F2N3-ciclo8.ts";
-import * as $48 from "./routes/planodeaula/F2N1.ts";
-import * as $49 from "./routes/planodeaula/F2N2.ts";
-import * as $50 from "./routes/planodeaula/F2N3.ts";
+import * as $8 from "./routes/conteudos/F2N3-ciclo3.ts";
+import * as $9 from "./routes/conteudos/F2N3-ciclo4.ts";
+import * as $10 from "./routes/conteudos/F2N3-ciclo5.ts";
+import * as $11 from "./routes/conteudos/F2N3-ciclo6.ts";
+import * as $12 from "./routes/conteudos/F2N3-ciclo7.ts";
+import * as $13 from "./routes/conteudos/F2N3-ciclo8.ts";
+import * as $14 from "./routes/fase2-repositorio.ts";
+import * as $15 from "./routes/hq/F2N1-ciclo1.ts";
+import * as $16 from "./routes/hq/F2N1-ciclo2.ts";
+import * as $17 from "./routes/hq/F2N1-ciclo3.ts";
+import * as $18 from "./routes/hq/F2N1-ciclo4.ts";
+import * as $19 from "./routes/hq/F2N1-ciclo5.ts";
+import * as $20 from "./routes/hq/F2N1-ciclo6.ts";
+import * as $21 from "./routes/hq/F2N1-ciclo7.ts";
+import * as $22 from "./routes/hq/F2N1-ciclo8.ts";
+import * as $23 from "./routes/infografico/F2N1-ciclo1.ts";
+import * as $24 from "./routes/infografico/F2N1-ciclo2.ts";
+import * as $25 from "./routes/infografico/F2N1-ciclo3.ts";
+import * as $26 from "./routes/infografico/F2N1-ciclo4.ts";
+import * as $27 from "./routes/infografico/F2N1-ciclo5.ts";
+import * as $28 from "./routes/infografico/F2N1-ciclo6.ts";
+import * as $29 from "./routes/infografico/F2N1-ciclo7.ts";
+import * as $30 from "./routes/infografico/F2N1-ciclo8.ts";
+import * as $31 from "./routes/infografico/F2N2-ciclo1.ts";
+import * as $32 from "./routes/infografico/F2N2-ciclo2.ts";
+import * as $33 from "./routes/infografico/F2N2-ciclo3.ts";
+import * as $34 from "./routes/infografico/F2N2-ciclo4.ts";
+import * as $35 from "./routes/infografico/F2N2-ciclo5.ts";
+import * as $36 from "./routes/infografico/F2N2-ciclo6.ts";
+import * as $37 from "./routes/infografico/F2N2-ciclo7.ts";
+import * as $38 from "./routes/infografico/F2N2-ciclo8.ts";
+import * as $39 from "./routes/infografico/F2N3-ciclo1.ts";
+import * as $40 from "./routes/infografico/F2N3-ciclo2.ts";
+import * as $41 from "./routes/infografico/F2N3-ciclo3.ts";
+import * as $42 from "./routes/infografico/F2N3-ciclo4.ts";
+import * as $43 from "./routes/infografico/F2N3-ciclo5.ts";
+import * as $44 from "./routes/infografico/F2N3-ciclo6.ts";
+import * as $45 from "./routes/infografico/F2N3-ciclo7.ts";
+import * as $46 from "./routes/infografico/F2N3-ciclo8.ts";
+import * as $47 from "./routes/planodeaula/F2N1.ts";
+import * as $48 from "./routes/planodeaula/F2N2.ts";
+import * as $49 from "./routes/planodeaula/F2N3.ts";
 
 const manifest = {
   routes: {
@@ -64,49 +63,48 @@ const manifest = {
     "./routes/conteudos/F2N2-ciclo7.ts": $5,
     "./routes/conteudos/F2N2-ciclo8.ts": $6,
     "./routes/conteudos/F2N3-ciclo1.ts": $7,
-    "./routes/conteudos/F2N3-ciclo2.ts": $8,
-    "./routes/conteudos/F2N3-ciclo3.ts": $9,
-    "./routes/conteudos/F2N3-ciclo4.ts": $10,
-    "./routes/conteudos/F2N3-ciclo5.ts": $11,
-    "./routes/conteudos/F2N3-ciclo6.ts": $12,
-    "./routes/conteudos/F2N3-ciclo7.ts": $13,
-    "./routes/conteudos/F2N3-ciclo8.ts": $14,
-    "./routes/fase2-repositorio.ts": $15,
-    "./routes/hq/F2N1-ciclo1.ts": $16,
-    "./routes/hq/F2N1-ciclo2.ts": $17,
-    "./routes/hq/F2N1-ciclo3.ts": $18,
-    "./routes/hq/F2N1-ciclo4.ts": $19,
-    "./routes/hq/F2N1-ciclo5.ts": $20,
-    "./routes/hq/F2N1-ciclo6.ts": $21,
-    "./routes/hq/F2N1-ciclo7.ts": $22,
-    "./routes/hq/F2N1-ciclo8.ts": $23,
-    "./routes/infografico/F2N1-ciclo1.ts": $24,
-    "./routes/infografico/F2N1-ciclo2.ts": $25,
-    "./routes/infografico/F2N1-ciclo3.ts": $26,
-    "./routes/infografico/F2N1-ciclo4.ts": $27,
-    "./routes/infografico/F2N1-ciclo5.ts": $28,
-    "./routes/infografico/F2N1-ciclo6.ts": $29,
-    "./routes/infografico/F2N1-ciclo7.ts": $30,
-    "./routes/infografico/F2N1-ciclo8.ts": $31,
-    "./routes/infografico/F2N2-ciclo1.ts": $32,
-    "./routes/infografico/F2N2-ciclo2.ts": $33,
-    "./routes/infografico/F2N2-ciclo3.ts": $34,
-    "./routes/infografico/F2N2-ciclo4.ts": $35,
-    "./routes/infografico/F2N2-ciclo5.ts": $36,
-    "./routes/infografico/F2N2-ciclo6.ts": $37,
-    "./routes/infografico/F2N2-ciclo7.ts": $38,
-    "./routes/infografico/F2N2-ciclo8.ts": $39,
-    "./routes/infografico/F2N3-ciclo1.ts": $40,
-    "./routes/infografico/F2N3-ciclo2.ts": $41,
-    "./routes/infografico/F2N3-ciclo3.ts": $42,
-    "./routes/infografico/F2N3-ciclo4.ts": $43,
-    "./routes/infografico/F2N3-ciclo5.ts": $44,
-    "./routes/infografico/F2N3-ciclo6.ts": $45,
-    "./routes/infografico/F2N3-ciclo7.ts": $46,
-    "./routes/infografico/F2N3-ciclo8.ts": $47,
-    "./routes/planodeaula/F2N1.ts": $48,
-    "./routes/planodeaula/F2N2.ts": $49,
-    "./routes/planodeaula/F2N3.ts": $50,
+    "./routes/conteudos/F2N3-ciclo3.ts": $8,
+    "./routes/conteudos/F2N3-ciclo4.ts": $9,
+    "./routes/conteudos/F2N3-ciclo5.ts": $10,
+    "./routes/conteudos/F2N3-ciclo6.ts": $11,
+    "./routes/conteudos/F2N3-ciclo7.ts": $12,
+    "./routes/conteudos/F2N3-ciclo8.ts": $13,
+    "./routes/fase2-repositorio.ts": $14,
+    "./routes/hq/F2N1-ciclo1.ts": $15,
+    "./routes/hq/F2N1-ciclo2.ts": $16,
+    "./routes/hq/F2N1-ciclo3.ts": $17,
+    "./routes/hq/F2N1-ciclo4.ts": $18,
+    "./routes/hq/F2N1-ciclo5.ts": $19,
+    "./routes/hq/F2N1-ciclo6.ts": $20,
+    "./routes/hq/F2N1-ciclo7.ts": $21,
+    "./routes/hq/F2N1-ciclo8.ts": $22,
+    "./routes/infografico/F2N1-ciclo1.ts": $23,
+    "./routes/infografico/F2N1-ciclo2.ts": $24,
+    "./routes/infografico/F2N1-ciclo3.ts": $25,
+    "./routes/infografico/F2N1-ciclo4.ts": $26,
+    "./routes/infografico/F2N1-ciclo5.ts": $27,
+    "./routes/infografico/F2N1-ciclo6.ts": $28,
+    "./routes/infografico/F2N1-ciclo7.ts": $29,
+    "./routes/infografico/F2N1-ciclo8.ts": $30,
+    "./routes/infografico/F2N2-ciclo1.ts": $31,
+    "./routes/infografico/F2N2-ciclo2.ts": $32,
+    "./routes/infografico/F2N2-ciclo3.ts": $33,
+    "./routes/infografico/F2N2-ciclo4.ts": $34,
+    "./routes/infografico/F2N2-ciclo5.ts": $35,
+    "./routes/infografico/F2N2-ciclo6.ts": $36,
+    "./routes/infografico/F2N2-ciclo7.ts": $37,
+    "./routes/infografico/F2N2-ciclo8.ts": $38,
+    "./routes/infografico/F2N3-ciclo1.ts": $39,
+    "./routes/infografico/F2N3-ciclo2.ts": $40,
+    "./routes/infografico/F2N3-ciclo3.ts": $41,
+    "./routes/infografico/F2N3-ciclo4.ts": $42,
+    "./routes/infografico/F2N3-ciclo5.ts": $43,
+    "./routes/infografico/F2N3-ciclo6.ts": $44,
+    "./routes/infografico/F2N3-ciclo7.ts": $45,
+    "./routes/infografico/F2N3-ciclo8.ts": $46,
+    "./routes/planodeaula/F2N1.ts": $47,
+    "./routes/planodeaula/F2N2.ts": $48,
+    "./routes/planodeaula/F2N3.ts": $49,
   },
   islands: {},
   baseUrl: import.meta.url,
